3d4412a0108cb8fe4feb47c9b8a2ebd7d851be3c
Kyle Fang <zhigang1992@gmail.com>
2026-01-14 13:55:56 +0800

    Revert "Add guidance to use AskUserQuestion for complex questions"
    This reverts commit 4b050ef341383d01ea7f81ad96346b1652966075.

--- sources/sync/prompt/systemPrompt.ts
+++ sources/sync/prompt/systemPrompt.ts
@@ -14,8 +14,6 @@ export const systemPrompt = trimIdent(`
     You must output this in the very end of your response, not inside of any other text. Do not wrap it into a codeblock. Always dedicate "<options>" and "</options>" to a dedicated line. Never output anything like "custom", user always have an option to send a custom message. Do not enumerate options in both text and options block.
     Always prefer to use the options mode to the text mode. Try to keep options minimal, better to clarify in a next steps.
 
-    For more complicated questions that require longer answers, free-form input, or multiple selections, use the AskUserQuestion tool instead of options.
-
     # Plan mode with options
 
     When you are in the plan mode, you must use the options mode to give the user a easy way to answer your questions if you know possible answers. Do not assume what is needed, when there is discrepancy between what you need and what you have, you must use the options mode.
